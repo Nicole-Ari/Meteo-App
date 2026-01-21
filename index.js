4aa42a66fb972eca5e8690957582e7bab740f505
ArisoaNC <rmnicole48@gmail.com>
2026-01-21 22:47:39 +0300

    fix: add loading, error state

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const wrapper = document.querySelector(".search-area");
+const container = document.querySelector(".container");
+const locations = document.querySelector(".location");
+const titles = document.querySelector(".titles");
+const errorMessage = document.querySelector(".errorMessage");
+const Dropdowncontainer = document.querySelector(".dropdown-container");
 const input = document.querySelector(".search-area input");
 const searchbtn = document.getElementById("searchbtn");
 const cityContainer = document.getElementById("cityContainer");
@@ -28,7 +33,7 @@ const currentDay = document.getElementById("currentDay");
 const d1 = document.getElementById("d1");
 const iconTemp = document.querySelector(".iconTemp");
 const cityNames = "https://api.turkiyeapi.dev/v1/provinces";
-var cityList = [];
+var cityList = {};
 var cityLat = 0;
 var cityLong = 0;
 var urlMetric = "";
@@ -55,52 +60,58 @@ const codes = {
   snow: [71, 73, 75, 77, 85, 86],
   storm: [95, 96, 99],
 };
+const loader = document.getElementById("loaderElement");
+const loadingContainer = document.querySelector(".loadingContainer");
+
+function showLoader() {
+  loader.hidden = false;
+}
+function showItems() {
+  titles.hidden = false;
+  container.hidden = false;
+  errorMessage.hidden = true;
+}
+function hideItems() {
+  titles.hidden = true;
+  container.hidden = true;
+  errorMessage.hidden = false;
+}
+showItems();
+
+function hideLoader() {
+  loader.hidden = true;
+}
+
+showLoader();
 /* prend la liste des villes */
 fetch(cityNames)
-  .then((res) => res.json())
-  .then(({ data }) =>
-    data.forEach((element) => {
-      const p = document.createElement("p");
-      p.className = "dropdown-item";
-      p.textContent = element.name;
-      p.addEventListener("click", (event) => {
-        searchContent.value = event.target.textContent;
-        getCoordonates(event.target.textContent);
-      });
-      cityContainer.appendChild(p);
-      date.textContent = new Date().toLocaleDateString("en-US", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
-    }),
-  )
-  .catch((err) => console.log(err));
+  .then((res) => {
+    if (res.status != 200) {
+      throw new Error(`HTTP ${res.status}`);
+    }
+    showItems();
 
-/* Retourne les coordonnées d'une ville*/
-const getCoordonates = (ct) => {
-  city = ct;
-  fetch(
-    `https://geocoding-api.open-meteo.com/v1/search?name=${ct}&country_code=TR&count=10&language=en&format=json`,
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      country = data.results[0].country;
-      cityLat = data.results[0].latitude;
-      cityLong = data.results[0].longitude;
-    })
-    .catch((error) => console.error("Error fetching geocoding data:", error));
-};
-
-const getLink = (lat, long) => {
-  let link = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
-  if (mode.textContent.trim() == "imperial") {
-    link = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m&timezone=auto`;
-  }
-  return link;
-};
-
+    return res.json();
+  })
+  .then(({ data }) => {
+    showItems();
+    cityList = Object.values(data).map((el) => el.name);
+    createCityDropdwon(cityList);
+    date.textContent = new Date().toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  })
+  .catch((err) => {
+    hideItems();
+    errorMessage.hidden = false;
+  })
+  .finally(() => {
+    hideLoader();
+    location.hidden = false;
+  });
 const changeMode = () => {
   if (mode.textContent.trim() == "imperial") {
     mode.textContent = " metric";
@@ -132,19 +143,102 @@ const changeMode = () => {
     });
   }
 };
-
-/* Affiche tous les données (Metric mode)*/
-const getWeatherData = (lat, long) => {
-  const link = getLink(lat, long);
+/* Retourne les coordonnées d'une ville*/
+const getCoordonates = (ct) => {
+  city = ct;
+  fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${ct}&country_code=TR&count=10&language=en&format=json`,
+  )
+    .then((response) => {
+      if (response.status != 200) {
+        throw new Error(`HTTP ${response.status}`);
+      }
+      showItems();
+      return response.json();
+    })
+    .then((data) => {
+      country = data.results[0].country;
+      cityLat = data.results[0].latitude;
+      cityLong = data.results[0].longitude;
+    })
+    .catch((error) => hideItems());
+};
+/* Affiche tous les données (Imperial mode) */
+const getImperialCurrentWeather = (lat, long, day) => {
+  changeMode();
+  initialisedData();
+  const imp = getLink(lat, long);
   if (searchContent.value.trim() != "Search for a place...") {
-    fetch(link)
-      .then((response) => response.json())
+    fetch(imp)
+      .then((response) => {
+        if (response.status != 200) {
+          throw new Error(`HTTP ${response.status}`);
+        }
+        showItems();
+        locations.hidden = false;
+        loadingContainer.hidden = true;
+        return response.json();
+      })
       .then((data) => {
         createCurrentWeather(data);
-        createDailyWeather(data);
-        createHourlyWeather(data);
+        createDailyWeather(data, day);
+        createHourlyWeather(data, day);
       })
-      .catch((error) => console.error("Error fetching weather data:", error));
+      .catch((error) => hideItems());
+  }
+};
+/* Affiche tous les données (Metric mode)*/
+const getWeatherData = (lat, long, day) => {
+  locations.hidden = true;
+  loadingContainer.hidden = false;
+  const link = getLink(lat, long);
+  initialisedData();
+  if (searchContent.value.trim() != "Search for a place...") {
+    fetch(link)
+      .then((response) => {
+        if (response.status != 200) {
+          throw new Error(`HTTP ${response.status}`);
+        }
+        locations.hidden = false;
+        loadingContainer.hidden = true;
+        return response.json();
+      })
+      .then((data) => {
+        createCurrentWeather(data);
+        createDailyWeather(data, day);
+        createHourlyWeather(data, day);
+      })
+      .catch((error) => hideItems());
+  }
+};
+
+const getLink = (lat, long) => {
+  let link = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
+  if (mode.textContent.trim() == "imperial") {
+    link = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m&timezone=auto`;
+  }
+  return link;
+};
+
+const createCityDropdwon = (data) => {
+  cityContainer.innerHTML = "";
+  if (data.length != 0) {
+    data.forEach((element) => {
+      const p = document.createElement("p");
+      p.className = "dropdown-item";
+      p.textContent = element;
+      p.addEventListener("click", (event) => {
+        searchContent.value = event.target.textContent;
+        getCoordonates(event.target.textContent);
+        closeDropown();
+      });
+      cityContainer.appendChild(p);
+    });
+  } else {
+    const p = document.createElement("p");
+    p.className = "dropdown-item notFound";
+    p.textContent = "No search result found!";
+    cityContainer.appendChild(p);
   }
 };
 
@@ -163,23 +257,28 @@ const createCurrentWeather = (data) => {
     `${mode.textContent.trim() == "imperial" ? " mm" : " in"}`;
 };
 const initialisedData = () => {
-  cityName.textContent = "";
-  countryName.textContent = "";
+  cityName.textContent = "-";
+  countryName.textContent = "-";
   temperature.textContent = "";
-  feelsLike.textContent = "";
-  humidity.textContent = "";
-  wind.textContent = "";
-  precipitation.textContent = "";
-  document.querySelectorAll(".day").innerHTML = "";
+  feelsLike.textContent = "-";
+  humidity.textContent = "-";
+  wind.textContent = "-";
+  precipitation.textContent = "-";
+  document.querySelectorAll(".day").forEach((el) => (el.innerHTML = ""));
+  document.querySelectorAll(".item").forEach((el) => (el.innerHTML = ""));
 };
 // Daily Weather
-const createDailyWeather = (data) => {
-  document.querySelectorAll(".day").innerHTML = "";
+const createDailyWeather = (data, day) => {
+  document.querySelectorAll(".day").forEach((el) => (el.innerHTML = ""));
   data.daily.time.forEach((dateStr, index) => {
     const dateObj = new Date(dateStr);
     const dayName = dates[dateObj.getDay()];
     const dayLogName = datesLong[dateObj.getDay()];
-    currentDay.textContent = index == 0 ? dayLogName : currentDay.textContent;
+    if (day != "-") {
+      currentDay.textContent = day;
+    } else {
+      currentDay.textContent = index == 0 ? dayLogName : currentDay.textContent;
+    }
     createDays(data.hourly);
     createDailyData(
       index,
@@ -219,18 +318,45 @@ const createDays = (data) => {
 };
 
 // Hourly weather
-const createHourlyWeather = (data) => {
+const createHourlyWeather = (data, day) => {
+  let times = [];
+  let newIndex = 0;
   hourlyData.innerHTML = "";
-  data.hourly.time.forEach((timeStr, index) => {
-    const dateObj = new Date(timeStr);
-    const dayName = datesLong[dateObj.getDay()];
-    createHourlyData(
-      dayName,
-      timeStr,
-      data.hourly.temperature_2m[index],
-      defineWeatherIcon(data.hourly.weather_code[index]),
-    );
-  });
+  if (day == "-") {
+    data.hourly.time.forEach((timeStr, index) => {
+      const dateObj = new Date(timeStr);
+      const dayName = datesLong[dateObj.getDay()];
+      createHourlyData(
+        dayName,
+        timeStr,
+        data.hourly.temperature_2m[index],
+        defineWeatherIcon(data.hourly.weather_code[index]),
+      );
+    });
+  } else {
+    if (hourlyData.innerHTML != "") {
+      hourlyData.innerHTML == "";
+    } else {
+      times = data.hourly.time.filter((timeStr) => {
+        const dateObje = new Date(timeStr);
+        const days = datesLong[dateObje.getDay()];
+        return days == day;
+      });
+      newIndex = data.hourly.time.findIndex((timeStr) => {
+        const dateObje = new Date(timeStr);
+        const days = datesLong[dateObje.getDay()];
+        return days == day;
+      });
+      times.forEach((timeStr, index) => {
+        createHourlyData(
+          day,
+          timeStr,
+          data.hourly.temperature_2m[newIndex + index],
+          defineWeatherIcon(data.hourly.weather_code[newIndex + index]),
+        );
+      });
+    }
+  }
 };
 
 const createHourlyData = (day, timeStr, deg, img) => {
@@ -241,7 +367,7 @@ const createHourlyData = (day, timeStr, deg, img) => {
     const ampm = hours >= 12 ? "PM" : "AM";
     const div = document.createElement("div");
     div.className = "item hour";
-    div.innerHTML = `<span><img src="${img}" alt="weather icon">${
+    div.innerHTML = `<span><img src="${img}" alt="weather icon" loading="lazy">${
       hours === 0 ? 12 : hours
     } ${ampm}</span>`;
     div.innerHTML += `<p>${deg}°</p>`;
@@ -281,28 +407,19 @@ const defineWeatherIcon = (code) => {
   return "assets/images/icon-overcast.webp";
 };
 
-/* Affiche tous les données (Imperial mode) */
-const getImperialCurrentWeather = (lat, long) => {
-  changeMode();
-  initialisedData();
-  const imp = getLink(lat, long);
-  if (searchContent.value.trim() != "Search for a place...") {
-    fetch(imp)
-      .then((response) => response.json())
-      .then((data) => {
-        createCurrentWeather(data);
-        createDailyWeather(data);
-        createHourlyWeather(data);
-      })
-      .catch((error) => console.log("error: ", error));
-  }
-};
-
 const unfocused = () => {
   const day = currentDay.textContent;
   document.querySelector(`.${day}`).classList.remove("active");
 };
-
+const onTextChange = (txt) => {
+  const citySelected = cityList.filter((el) =>
+    el.toLowerCase().includes(txt.toLowerCase()),
+  );
+  createCityDropdwon(citySelected);
+};
+const refresh = () => {
+  window.location.reload();
+};
 const observer = new MutationObserver(() => {
   if (currentDay.textContent !== "-") {
     dropdownDaysItems.forEach((el) => {
@@ -319,35 +436,43 @@ observer.observe(currentDay, {
   subtree: true,
 });
 
+cityContainer.addEventListener("focusout", closeDropown);
+dropsettings.addEventListener("focusout", closeSettings);
+settings.addEventListener("focus", openSettings);
 searchbtn.addEventListener("click", () => {
   first.forEach((el) => el.classList.add("loading"));
-  getWeatherData(cityLat, cityLong);
+  const day =
+    currentDay.textContent.trim() != "-" ? currentDay.textContent.trim() : "-";
+  getWeatherData(cityLat, cityLong, day);
 });
 
-wrapper.addEventListener("focus", () => {
+Dropdowncontainer.addEventListener("focusout", (e) => {
+  /*verifie si le nouvel élément focus est toujours dans Dropdowncontainer*/
+  if (!Dropdowncontainer.contains(e.relatedTarget)) {
+    document.querySelector(".dropdown-items").style.visibility = "hidden";
+  }
+});
+
+searchContent.addEventListener("focus", () => {
   openDropown();
+  if (searchContent.value != "") {
+    onTextChange(searchContent.value);
+  }
 });
 
-cityContainer.addEventListener("focusout", () => {
-  closeDropown();
+searchContent.addEventListener("input", (e) => {
+  onTextChange(e.target.value);
 });
 
 input.addEventListener("mouseleave", () => {
   searchbtn.style.background = "";
 });
 
-dropsettings.addEventListener("focusout", () => {
-  closeSettings();
-});
-
-settings.addEventListener("focus", () => {
-  openSettings();
-});
-settings.addEventListener("focusout", () => {
-  closeSettings();
-});
 switchBtn.addEventListener("click", () => {
-  getImperialCurrentWeather(cityLat, cityLong);
+  const day =
+    currentDay.textContent.trim() != "-" ? currentDay.textContent.trim() : "-";
+  getImperialCurrentWeather(cityLat, cityLong, day);
+  closeSettings();
 });
 
 days.addEventListener("focus", () => {
@@ -365,7 +490,6 @@ days.addEventListener("focusout", () => {
 
 function openDropown() {
   document.querySelector(".dropdown-items").style.visibility = "visible";
-  document.querySelector(".dropdown-items").focus();
 }
 
 function closeDropown() {
